feb052ea03509950eac49cd06e05bf36b0c85048
ryuya-matsunawa <ruymtnw@gmail.com>
2024-07-18 12:54:30 +0900

    fix: link

--- link.sh
+++ link.sh
@@ -3,8 +3,8 @@
 # シンボリックリンク
 ln -sf ~/works/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/works/dotfiles/git/.gitconfig ~/.gitconfig
-ln -sf ~/works/dotfiles/starship.toml ~/.config/starship.toml
+ln -sf ~/works/dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -sf ~/works/dotfiles/hammerspoon ~/.hammerspoon
-ln -sf ~/works/dotfiles/vscodium/settings.json ~/.vscode/settings.json
+ln -sf ~/works/dotfiles/vscode/settings.json ~/.vscode/settings.json
 
 source ~/.zshrc
